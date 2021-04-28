22e06f0feb39a78736648557983290d119888fa5
shawndevp <shawn.ornerstedt99@hotmail.com>
2021-04-28 11:38:53 +0200

    first strapi-cms commit for tattooshop

---
+++ tattoshopbackend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dd7909b2577b47e43295e42566e3f998'),
+    },
+  },
+});
---
+++ tattoshopbackend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cabce6c9-ef6f-4e90-92f4-63a97dd6fd4b'
+};
